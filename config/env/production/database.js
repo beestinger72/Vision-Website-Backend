e0d6cda776096adea9c12cfb9b749d25320ec272
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-03 13:26:13 +0100

    #

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
   connection: {
       client: 'postgresql',
     connection: {
+       client: 'postgresql',
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
