12e0bc1de0079ef8e646f2bedf9d02e621065271
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-10 15:12:55 +0100

    Revert "#"
    This reverts commit 48a7e52cda36e47c7ef0c068197cc724442de500.

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,34 +1,26 @@
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', 'app-42c4a654-cde9-4905-8787-77b8c7f0b9eb-do-user-13845580-0.b.db.ondigitalocean.com'),
-        port: env.int('DATABASE_PORT', 25060),
-        database: env('DATABASE_NAME', 'db'),
-        username: env('DATABASE_USERNAME', 'db'),
-        password: env('DATABASE_PASSWORD', 'AVNS_VJIdCceS_hybYdsvRN3'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-      options: {}
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      
+       ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates },
     },
-  },
-  debug: false,
-  acquireConnectionTimeout: 600000,
-  pool: {
-    min: 0,
-    max: 100,
-    acquireTimeoutMillis: 300000,
-    createTimeoutMillis: 300000,
-    destroyTimeoutMillis: 50000,
-    idleTimeoutMillis: 300000,
-    reapIntervalMillis: 10000,
-    createRetryIntervalMillis: 2000,
-    propagateCreateError: false,
+    },
+    debug: false,
   },
 });
+
+
+
 
 
 
