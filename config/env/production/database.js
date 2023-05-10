4893b89fae0cb682297683b95f2a46fb67e06380
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-10 17:08:41 +0100

    Revert "#"
    This reverts commit 48a7e52cda36e47c7ef0c068197cc724442de500.

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,40 +1,25 @@
 
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', '127.0.0.1'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'strapi'),
-//       user: env('DATABASE_USERNAME', 'strapi'),
-//       password: env('DATABASE_PASSWORD', 'strapi'),
-//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      
-//        ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates },
-//     },
-//     },
-//     debug: false,
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
+      port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      
+       ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates },
+    },
     },
     debug: false,
   },
 });
+
+
 
 
 
