1b55921dca9ac20df1d960267a24a25b72c8db92
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-10 14:07:59 +0100

    #

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,19 +1,15 @@
 module.exports = ({ env }) => ({
+  // ...
   upload: {
     config: {
-      provider: "strapi-provider-upload-dos",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
-        // key: process.env.DO_SPACE_ACCESS_KEY,
-        // secret: process.env.DO_SPACE_SECRET_KEY,
-        // endpoint: process.env.DO_SPACE_ENDPOINT,
-        // space: process.env.DO_SPACE_BUCKET,
-        // directory: process.env.DO_SPACE_DIRECTORY,
-        // cdn: process.env.DO_SPACE_CDN,
-        key: env("DO_SPACE_ACCESS_KEY"),
-        secret: env("DO_SPACE_SECRET_KEY"),
-        endpoint: env("DO_SPACE_ENDPOINT"),
-        space: env("DO_SPACE_BUCKET"),
+        key: process.env.DO_SPACE_ACCESS_KEY,
+        secret: process.env.DO_SPACE_SECRET_KEY,
+        endpoint: process.env.DO_SPACE_ENDPOINT,
+        space: process.env.DO_SPACE_BUCKET
       },
     },
   },
+  // ...
 });
