8bee5f870baa4476aaccc2d6a31d1d31ba06798e
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-10 14:45:14 +0100

    ##update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,8 @@
-
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
+      connector: 'bookshelf',
       settings: {
         client: 'postgres',
         host: env('DATABASE_HOST', 'app-42c4a654-cde9-4905-8787-77b8c7f0b9eb-do-user-13845580-0.b.db.ondigitalocean.com'),
@@ -15,25 +11,24 @@ module.exports = ({ env }) => ({
         username: env('DATABASE_USERNAME', 'db'),
         password: env('DATABASE_PASSWORD', 'AVNS_VJIdCceS_hybYdsvRN3'),
         ssl: env.bool('DATABASE_SSL', true),
-        
       },
       options: {}
     },
-  }, debug: false,
-        acquireConnectionTimeout: 600000,
-    pool: {
-      min: 0,
-      max: 100,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 50000,
-      idleTimeoutMillis: 300000,
-      reapIntervalMillis: 10000,
-      createRetryIntervalMillis: 2000,
-      propagateCreateError: false,
-    },
+  },
+  debug: false,
+  acquireConnectionTimeout: 600000,
+  pool: {
+    min: 0,
+    max: 100,
+    acquireTimeoutMillis: 300000,
+    createTimeoutMillis: 300000,
+    destroyTimeoutMillis: 50000,
+    idleTimeoutMillis: 300000,
+    reapIntervalMillis: 10000,
+    createRetryIntervalMillis: 2000,
+    propagateCreateError: false,
+  },
 });
-
 
 
 
