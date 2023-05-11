2fe44bda403df9ab6f83d28c896962ba75188c0c
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-11 15:30:27 +0100

    #

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,28 +1,31 @@
 module.exports = ({ env }) => ({
-client: 'postgres',
   connection: {
-
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+    ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      connection: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'app-42c4a654-cde9-4905-8787-77b8c7f0b9eb-do-user-13845580-0.b.db.ondigitalocean.com'),
-        port: env.int('DATABASE_PORT', 25060),
-        database: env('DATABASE_NAME', 'db'),
-        username: env('DATABASE_USERNAME', 'db'),
-        password: env('DATABASE_PASSWORD', 'AVNS_VJIdCceS_hybYdsvRN3'),
-        schema: env('DATABASE_SCHEMA', 'public'),
-        ssl: { rejectUnauthorized: false } 
-
-},
-acquireConnectionTimeout: 1000000,
-pool: {
-min: 0,
-max: 5,
-acquireTimeoutMillis: 300000,
-createTimeoutMillis: 300000,
-destroyTimeoutMillis: 300000,
-idleTimeoutMillis: 30000,
-reapIntervalMillis:1000,
-createRetryIntervalMillis: 2000
-},
-debug: false,
-});
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+             ca: env('DATABASE_CA')
+          },
+        },
+        debug: false,
+      },
+    },
+    debug: false,
+  }
+},});
+    
 
