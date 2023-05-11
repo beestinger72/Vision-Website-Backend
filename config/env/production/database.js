70f5587ffa915d525736e3897953f3763c75f838
beestinger72 <129185409+beestinger72@users.noreply.github.com>
2023-05-11 15:03:15 +0100

    #

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
+client: 'postgres',
+  connection: {
+
         client: 'postgres',
         host: env('DATABASE_HOST', 'app-42c4a654-cde9-4905-8787-77b8c7f0b9eb-do-user-13845580-0.b.db.ondigitalocean.com'),
         port: env.int('DATABASE_PORT', 25060),
@@ -11,9 +9,20 @@ module.exports = ({ env }) => ({
         username: env('DATABASE_USERNAME', 'db'),
         password: env('DATABASE_PASSWORD', 'AVNS_VJIdCceS_hybYdsvRN3'),
         schema: env('DATABASE_SCHEMA', 'public'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-      options: {}
-    },
-  },
+        ssl: env.bool('DATABASE_SSL', false),
+
+},
+acquireConnectionTimeout: 1000000,
+pool: {
+min: 0,
+max: 5,
+acquireTimeoutMillis: 300000,
+createTimeoutMillis: 300000,
+destroyTimeoutMillis: 300000,
+idleTimeoutMillis: 30000,
+reapIntervalMillis:1000,
+createRetryIntervalMillis: 2000
+},
+debug: false,
 });
+
